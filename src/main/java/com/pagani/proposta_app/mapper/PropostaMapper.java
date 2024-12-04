09d2341cf50cc6eafdbfb9e280e1c957bdc5a295
joaovitorpagani99 <joaovitorpagani7@hotmail.com>
2024-12-04 08:23:22 -0300

    feat: add funcionalidade de salvar no service e usando o mapper

--- src/main/java/com/pagani/proposta_app/mapper/PropostaMapper.java
+++ src/main/java/com/pagani/proposta_app/mapper/PropostaMapper.java
@@ -2,12 +2,16 @@ package com.pagani.proposta_app.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 import com.pagani.proposta_app.DTO.PropostaRequestDTO;
+import com.pagani.proposta_app.DTO.PropostaResponseDTO;
 import com.pagani.proposta_app.entity.Proposta;
 
 @Mapper
 public interface PropostaMapper {
+	
+	PropostaMapper INSTANCE = Mappers.getMapper(PropostaMapper.class);
 
 	@Mapping(target = "usuario.nome", source = "nome")
 	@Mapping(target = "usuario.sobrenome", source = "sobrenome")
@@ -19,4 +23,12 @@ public interface PropostaMapper {
 	@Mapping(target = "integrada" , ignore = true)
 	@Mapping(target = "observacao" , ignore = true)
 	Proposta convertDtoToProposta(PropostaRequestDTO propostaRequestDto);
+	
+	
+	@Mapping(target = "nome", source = "usuario.nome")
+	@Mapping(target = "sobrenome", source = "usuario.sobrenome")
+	@Mapping(target = "telefone", source = "usuario.telefone")
+	@Mapping(target = "cpf", source = "usuario.cpf")
+	@Mapping(target = "renda", source = "usuario.renda")
+	PropostaResponseDTO convertEntityToDTO(Proposta proposta);
 }
--- src/main/java/com/pagani/proposta_app/service/PropostaService.java
+++ src/main/java/com/pagani/proposta_app/service/PropostaService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.pagani.proposta_app.DTO.PropostaRequestDTO;
 import com.pagani.proposta_app.DTO.PropostaResponseDTO;
 import com.pagani.proposta_app.entity.Proposta;
+import com.pagani.proposta_app.mapper.PropostaMapper;
 import com.pagani.proposta_app.repository.PropostaRepository;
 
 @Service
@@ -15,7 +16,8 @@ public class PropostaService {
 	private PropostaRepository repository;
 
 	public PropostaResponseDTO save(PropostaRequestDTO requestDTO) {
-		repository.save(new Proposta());
-		return null;
+		Proposta proposta = PropostaMapper.INSTANCE.convertDtoToProposta(requestDTO);
+		repository.save(proposta);
+		return PropostaMapper.INSTANCE.convertEntityToDTO(proposta);
 	}
 }
